6392a5847a15ea45b72bae061747b13450ba028a
rahullbyte <errahul.rp@gmail.com>
2025-04-30 17:24:28 +0530

    Added conditional statements

---
+++ conditional.js
@@ -0,0 +1,19 @@
+//conditional statements
+
+// let age = 25;
+
+// if (age > 18) {
+//   console.log("User can able to vote");
+// }
+
+
+let mode = "dark";
+let color;
+
+if(mode === "dark"){
+    color = 'black'
+}else{
+    color = "white"
+}
+
+console.log(color);
---
+++ script.js
@@ -0,0 +1,53 @@
+// //Arithmetic Operators
+
+// let a = 5;
+// let b = 2;
+
+// console.log(" a = ", a, "& b =", b);
+// console.log(" a + b =", a + b);
+// console.log(" a -b =", a - b);
+// console.log(" a * b= ", a * b);
+// console.log(" a / b=", a / b);
+// console.log(" a % b=", a % b);
+// console.log(" a ** b=", a ** b);
+
+//Assignment operators
+// let a =5 
+// let b =2
+
+// a += b;
+
+// console.log("a =", a);
+
+
+//comparison operators
+
+// let a = 5;
+// let b = 2;
+
+// console.log(" 5 == 2", a ==b);
+
+
+// let a = "2";
+// let b = "2";
+
+// console.log(" 2 == 2", a ===b);
+
+// let a = 3;
+// let b = 2;
+
+// console.log(" 3 >= 2", a >=b);
+
+//Logical operators
+
+// let a = 2;
+// let b = 5;
+
+// console.log("&&", a < 5 && b ===5);
+
+
+// let a = 2;
+// let b = 5;
+
+// console.log("||", a < 5 || b ===5);
+
