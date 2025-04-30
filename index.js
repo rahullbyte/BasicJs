8c72fb2925d424843d1150daa99f1c123ac85cbf
rahullbyte <errahul.rp@gmail.com>
2025-04-30 12:02:32 +0530

    variables In JS

---
+++ index.js
@@ -0,0 +1,88 @@
+// console.log("Welcome to Apna College!");
+// console.log("Rahul")
+// console.log("I Love JS")
+
+// Fullname = "Tony Stark"
+
+// console.log(Fullname);
+
+// age =24
+
+// console.log(age);
+
+// price = 99.99
+
+// console.log(price);
+
+// x = null;
+
+// y = undefined;
+
+// z = NaN;
+
+// isFollow = true;
+
+// console.log(x, y, z);
+
+// for (var i = 0; i < 3; i++) {
+//     setTimeout(() => console.log(i), 1000);
+//   }
+
+// for (let i = 0; i < 3; i++) {
+//     setTimeout(() => console.log(i), 1000);
+//   }
+
+// console.log(a);
+
+// // var a = 3
+// console.log(typeof undeclaredVar);
+
+// var a = 1;
+// function outer() {
+//   var a = 2;
+//   function inner() {
+//     console.log(a);
+//   }
+//   return inner;
+// }
+// const func = outer();
+// func();
+
+// console.log(x);
+// var x = 5;
+// let y = 6;
+
+// var a = 20;
+// let a = 10;
+
+// console.log(a);
+
+// let x ;
+
+// console.log(typeof x);
+
+// const student ={
+//     FullName: "Rahul Patil",
+//     age : 20,
+//     cgpa: 8.9,
+//     isPass: true
+// }
+
+// student.isPass = 8.9
+
+// console.log(student.isPass);
+
+// const product = {
+//   title: "Parker Jotter Standard CT Ball Pen",
+//   color: "Black",
+//   Starts: 4,
+//   reviews: 7002,
+//   Price: 285,
+//   OfferPercentage: 5,
+//   img: "/images/pen.png",
+//   isDeal: true,
+// };
+
+// product.Price = product.Price - (product.Price * product.OfferPercentage/ 100)
+
+// console.log(product.Price);
