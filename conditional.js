0a9b39f4e88cd41b87a2de506494122b404be26a
rahullbyte <errahul.rp@gmail.com>
2025-05-01 16:20:34 +0530

    Conditional Statements

--- conditional.js
+++ conditional.js
@@ -6,14 +6,66 @@
 //   console.log("User can able to vote");
 // }
 
+// let mode = "dark";
+// let color;
 
-let mode = "dark";
-let color;
+// if(mode === "dark"){
+//     color = 'black'
+// }else{
+//     color = "white"
+// }
 
-if(mode === "dark"){
-    color = 'black'
-}else{
-    color = "white"
-}
+// console.log(color);
 
-console.log(color);
+//Odd or even
+
+// let num = 7;
+
+// if (num % 2 ===0 ){
+//     console.log("this is even Number:", num);
+// }else{
+//     console.log("This is odd Number:", num);
+
+// }
+
+// let money = 30000;
+
+// if (money === 20000) {
+//   console.log("buy House");
+// } else if (money === 12000) {
+//   console.log("buy bike");
+// } else if (money <= 40000) {
+//   console.log("Meaning less life");
+// } else if (money >= 40000) {
+//   console.log("Enjoy life");
+// } else {
+//   console.log("Fuck You");
+// }
+
+// let age = 20;
+
+// const results = age>= 18 ? 'adult': 'not Adult'
+
+// console.log(results);
+
+// let Number = prompt("Put the Number")
+
+// console.log(Number% 5 ===0);
+
+// let marks = prompt("Put a marks here:");
+
+// let grade;
+
+// if (marks >= 90 && marks <= 100) {
+//   grade = "A";
+// } else if (marks >= 70 && marks <= 89) {
+//   grade = "B";
+// } else if (marks >= 60 && marks <= 79) {
+//   grade = "C";
+// } else if (marks >= 50 && marks <= 69) {
+//   grade = "D";
+// } else if (marks >= 0 && marks <= 49) {
+//   grade = "F";
+// }
+
+// console.log("Received Grade:", grade);
