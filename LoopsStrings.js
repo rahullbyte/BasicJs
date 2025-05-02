48f6814f4f385266124a76356f271365a0ea4073
rahullbyte <errahul.rp@gmail.com>
2025-05-02 16:35:48 +0530

    Loops Strings

---
+++ LoopsStrings.js
@@ -0,0 +1,107 @@
+//For Loop
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log("Apna Collage");
+// }
+
+//While Loop
+
+// let i = 1;
+
+// while (i <= 10) {
+//   console.log("Apna College.");
+//   i++;
+// }
+
+//Do While Loop
+
+// let i = 1;
+
+// do {
+//   console.log("Apna College.");
+//   i++;
+// } while (i <= 10);
+
+// let i = 1;
+
+// do {
+//   console.log("i =", i);
+//   i++;
+// } while (i <= 5);
+
+//For of Loop
+
+// let string = "RahulPatil"
+
+// for(let i of string){
+//     console.log("i = ", i);
+// }
+
+// let a = undefined;
+
+// console.log(typeof a);
+
+// let count = 0;
+// while (count < 3) {
+//   console.log(count);
+//   count++;
+// }
+
+// let a = 1
+// let b = a++
+
+// console.log(a, b);
+
+// let sum = 0
+
+// for( let i =0; i<=5; i++){
+//     sum = sum + i
+// }
+
+// console.log(sum);
+
+// let count = 0;
+// while (count < 3) {
+//   count++;
+//   if (count === 2) {
+//     continue;
+//   }
+//   console.log(count);
+// }
+
+// let a = 5;
+// if (a == 5) {
+//   console.log("Yes");
+// } else {
+//   console.log("No");
+// }
+
+// let str = ""
+
+// console.log(str.length);
+
+// let a =10;
+
+// for(let i = 1; i<=10; i++){
+//     if(i%2===0){
+//         console.log(i);
+//     }
+// }
+
+// for(let i =1; i<=100; i++){
+
+//     if(i %2 ===0){
+//         console.log("Even Number:", i);
+//     }
+// }
+
+// let SecreteValue = 25;
+
+// const promptValue = prompt("Guess the Number");
+
+// while(SecreteValue !== promptValue){
+//     promptValue = prompt("You Entered wrong Number, Guess again:")
+// }
+
+// console.log("Correct");
+
