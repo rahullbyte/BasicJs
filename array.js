6e2df5de5bc6e62469753a51e20c99bc66f3d8d2
rahullbyte <errahul.rp@gmail.com>
2025-05-06 10:54:39 +0530

    Array

---
+++ array.js
@@ -0,0 +1,22 @@
+// let num =[1,2, 3,4, 5, 6, 7 ]
+
+// num.push(8)
+
+// console.log(num);
+
+
+// let num = [1,2,3,4,5,6,7,8,9]
+
+// num.pop()
+
+// console.log(num);
+
+
+//  let num = [1,2,3,4,5,6,7,8,9]
+
+//  num.toString()
+
+//  console.log(num);
+ 
+
+
